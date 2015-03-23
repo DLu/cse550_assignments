e4a2148474eb9ee81a794db7eeac12b48a2cf346
David Lu <davidvlu@gmail.com>
2015-03-23 12:59:49 -0500

    Assignment 03 structure

---
+++ assignment03/src/assignment03/vector.py
@@ -0,0 +1,8 @@
+def to_vector(x, y):
+    return 0.0, 0.0
+    
+def add_vectors(v1, v2, scale=1.0, power=1.0):
+    return v1
+    
+def get_command(pose, vector):
+    return 0.0, 0.0
---
+++ assignment03/src/avoid.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+import roslib; roslib.load_manifest('assignment03')
+
+
